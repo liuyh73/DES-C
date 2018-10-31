1cfd31d6e0f7452e75c680cc33fe8fed13881523
liuyh73 <7home7Tmade>
2018-11-01 00:06:29 +0800

    the use of des

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "des.hpp"
 #include <string>
-string key = "1101001010110101100110110111011110011001111010001101111001110110";
-string clear_text = "1111111011011100101110101001100001110110010101000011001000010000";
+using std::cin;
+using std::cout;
+using std::endl;
+string key; 
+string clear_text;
 int main(){
-	des *desInstance = new des(key);
+	cout<<"输入密钥：";
+	cin >> key;
+	cout<<"输入明文：";
+	cin >> clear_text;
+	des* desInstance = new des(key);
 	string cipher_text = desInstance->Encrypt(clear_text);
-	printf("%s\n", cipher_text);
-
+	cout<<"加密后的密文："<<cipher_text<<endl;
+	string plain_text = desInstance->Decrypt(cipher_text);
+	cout<<"解密后的明文："<<plain_text<<endl;
+	cout<<"clear_text == plain_text: "<<(clear_text == plain_text ? "true" : "false")<<endl;
 	delete desInstance;
 }
